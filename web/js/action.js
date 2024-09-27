7ba1a647102258eb3862f4e9ffce9f16d7ca6280
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-09-28 00:48:09 +0200

    Orden, limpieza del action.js + reemplazo de onclicks por eventListeners

--- web/js/action.js
+++ web/js/action.js
@@ -6,31 +6,25 @@ fetch('../back/getPreguntes.php?quantPreg='+quantPreg)
   jugar(dataRecibida);
 });
 
+let data;
 let iterador = 0; // Itera por cada pregunta
 window.onload = updateClock; //Al cargar la página web, comience la función del contador
 let totalTime = 5; // Duración del contador
-
-function jugar(dataRecibida){
-  data = dataRecibida
-  mostrarPreguntes(iterador);
-}
-
-// ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ARREGLAR ESTAS COSAS, HAY QUE EVITAR EL USO DE VARIABLES GLOBALES!!!!!!!!!!!!!!!!11
-//Declaramos los diferentes contenedores y el objeto que ºalmacenará información de la partida
-let enviarScore = document.getElementById('enviarScore');
-let playAgain = document.getElementById('playAgain');
-let subcontainer = document.getElementById('subcontainer');
-
 let estatDeLaPartida = {
   rtasFetas: new Array()
 };
+
+function jugar(dataRecibida){
+  data = dataRecibida;
+  mostrarPreguntes(iterador);
+}
 
 function mostrarPreguntes(indice){
   let htmlString = '';
   setTimeout(() => {
     document.getElementById("pregunta").innerHTML = data[indice].pregunta;
     for(let j = 0; j < data[indice].respostes.length; j++){
-      htmlString += `<button class="btn" id="${j}" name="boton" onclick="pulsar(${indice},${j})">${data[indice].respostes[j]}</button>`
+      htmlString += `<button class="btn" id=${j} idResp="${j}" name="boton">${data[indice].respostes[j]}</button>`
     }
     document.getElementById("respuestas").innerHTML = htmlString;
   }, 200);
@@ -52,19 +46,25 @@ function updateClock(){
     .then(response => response.json())
     .then(score => {
       htmlString = `<h2 class="tiempoAcabado">El tiempo se ha acabado</h2><h3 id="rtas">Respuestas correctas: ${score.numOk} / ${score.total}</h3>`;
-      subcontainer.innerHTML = htmlString;
+      document.getElementById('subcontainer').innerHTML = htmlString;
       console.log(score)
     })
     .catch(error => console.log('Error: '+error));
 
-    playAgain.classList.remove("hidden");
-    enviarScore.classList.remove("hidden");
+    document.getElementById('playAgain').classList.remove("hidden");
+    document.getElementById('enviarScore').classList.remove("hidden");
   }
 }
 
-// Acción que realiza cada vez que se pulsa un botón
-function pulsar(i,j){
-  if(iterador < data.length -1){
+document.getElementById('respuestas').addEventListener('click', e => {
+  if(e.target.classList.contains('btn')){
+    pulsar(e.target.getAttribute('idResp'));
+  }
+});
+
+// REACCIÓN AL PULSAR
+function pulsar(j){
+  if(iterador < data.length-1){
     // deshabilitar que pueda tocar otras opciones al pulsar el botón
     document.querySelectorAll(".btn").forEach(boton => {
       boton.classList.add("disabled");
@@ -92,13 +92,13 @@ function pulsar(i,j){
     })
     .catch(error => console.log("Error: " + error));
 
-    //document.getElementById(j).style.background = "#fdfd80";
     iterador++;
     mostrarPreguntes(iterador);
   }
 }
 
-playAgain.addEventListener("click", reiniciar);
+
+document.getElementById('playAgain').addEventListener("click", reiniciar);
 
 function reiniciar(){
   fetch('../back/sessiondestroy.php')
