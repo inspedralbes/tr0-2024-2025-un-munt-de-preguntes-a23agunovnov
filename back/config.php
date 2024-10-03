81b7849e3f1a2a1f8b02f3fa90fc97bd6f237caa
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-10-03 12:02:17 +0200

    Cambios test Hestia #03

--- back/config.php
+++ back/config.php
@@ -1,10 +1,10 @@
 <?php
-    $host = "localhost";
-    $user = "preguntas";
-    $pass = "12345";
-    $dbname = "TR0";
     // $host = "localhost";
-    // $user = "a23agunovnov_root";
-    // $pass = "Agustin2011";
-    // $dbname = "a23agunovnov_TR0";
+    // $user = "preguntas";
+    // $pass = "12345";
+    // $dbname = "TR0";
+    $host = "localhost";
+    $user = "a23agunovnov_root";
+    $pass = "Agustin2011";
+    $dbname = "a23agunovnov_TR0";
 ?>
